21ad49ee7ccefcc5b3b30259dae56971338bc2b6
laarififouad-pixel <laarifi.fouad@gmail.com>
2026-04-07 21:15:04 +0100

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+
+<header class="navbar">
+<img src="/assets/images/logo.jpg" class="logo">
+
+<nav>
+<a href="/">Home</a>
+<a href="/cars.php">Cars</a>
+<a href="/booking.php">Booking</a>
+</nav>
+</header>
