8096abb038aa7c75ee095133daf5db630f01d952
laarififouad-pixel <laarifi.fouad@gmail.com>
2026-04-07 21:18:23 +0100

    Create  booking.php

---
+++ booking.php
@@ -0,0 +1,12 @@
+<form action="send.php" method="POST">
+
+<input name="name" placeholder="Name"><br>
+<input name="email" placeholder="Email"><br>
+
+<select name="location">
+<option>Fes City</option>
+<option>Fes Airport</option>
+</select>
+
+<button>Book</button>
+</form>
